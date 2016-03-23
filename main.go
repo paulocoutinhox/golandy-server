93250e423618e2f752f4624228011745d7fb6497
Paulo Coutinho <paulo@prsolucoes.com>
2016-03-23 00:05:45 -0300

    ajustes no servidor ws

--- main.go
+++ main.go
@@ -9,49 +9,89 @@ import (
 	"sync"
 	"fmt"
 	"encoding/json"
+	"time"
 )
+
+var appVersion = "1.0"
+var validateOrigin = false
 
 var wsUpgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
-		// valida a origem - desabilitado por enquanto
-		/*
-		if r.Header.Get("Origin") != "http://" + r.Host {
-			return false
+		if validateOrigin {
+			if r.Header.Get("Origin") != "http://" + r.Host {
+				return false
+			}
 		}
-		*/
+
 		return true
 	},
 }
 
 var Players = make([]*Player, 0)
 
-type Message struct {
-	Y      int    // current Y position
-	X      int    // as above
-	Id     string // the id of the player that sent the message
-	New    bool   // true if this player just connected so we know when to
-                  // spawn a new sprite on the screens of the other players. for all subsequent
-                  // messages it's false
-	Online bool   // true if the player is no longer connected so the frontend
-                  // will remove it's sprite
+type SimpleMessage struct {
+	Type string `json:"type"`
+}
+
+type PlayerPositionMessage struct {
+	Type   string `json:"type"`
+	Id     string `json:"id"`
+	X      int `json:"x"`
+	Y      int `json:"y"`
+	New    bool `json:"new"`
+	Online bool `json:"online"`
+}
+
+type PlayerInvalidPositionMessage struct {
+	Type string `json:"type"`
+	Id   string `json:"id"`
+	X    int `json:"x"`
+	Y    int `json:"y"`
+}
+
+type PlayerDataMessage struct {
+	Type          string `json:"type"`
+	Id            string `json:"id"`
+	X             int `json:"x"`
+	Y             int `json:"y"`
+	CharType      string `json:"charType"`
+	Direction     int `json:"direction"`
+	MovementDelay float64 `json:"movementDelay"`
 }
 
 type Player struct {
-	Y      int             // Y position of the player
-	X      int             // X position
-	Id     string          // a unique id to identify the player by the frontend
-	Socket *websocket.Conn // websocket connection of the player
-	mu     sync.Mutex
+	Id               string
+	X                int
+	Y                int
+	CharType         string
+	Direction        int
+	MovementDelay    float64
+	LastMovementTime time.Time
+
+	Socket           *websocket.Conn
+	mu               sync.Mutex
 }
 
 func debug(message string) {
 	log.Printf("> %s\n", message);
 }
 
-func (p *Player) position(new bool) Message {
-	return Message{X: p.X, Y: p.Y, Id: p.Id, New: new, Online: true}
+func (p *Player) createSimpleMessage(messageType string) SimpleMessage {
+	return SimpleMessage{Type: messageType}
+}
+
+func (p *Player) createPositionMessage(new bool) PlayerPositionMessage {
+	return PlayerPositionMessage{Type: "pos", X: p.X, Y: p.Y, Id: p.Id, New: new, Online: true}
+}
+
+func (p *Player) createInvalidPositionMessage() PlayerInvalidPositionMessage {
+	return PlayerInvalidPositionMessage{Type: "pos-invalid", X: p.X, Y: p.Y, Id: p.Id}
+}
+
+func (p *Player) createPlayerDataMessage() PlayerDataMessage {
+	return PlayerDataMessage{Type: "player-data", X: p.X, Y: p.Y, Id: p.Id, CharType: p.CharType, Direction: p.Direction, MovementDelay: p.MovementDelay}
 }
 
 func (p *Player) send(v interface{}) error {
@@ -60,7 +100,16 @@ func (p *Player) send(v interface{}) error {
 	return p.Socket.WriteJSON(v)
 }
 
-// a slice of *Players which will store the list of connected players
+func (p *Player) updateLastMovementTime() {
+	p.LastMovementTime = time.Now()
+}
+
+func (p *Player) canMove() bool {
+	seconds := time.Now().Sub(p.LastMovementTime).Seconds()
+	ms := seconds * 1000
+	return (ms > p.MovementDelay)
+}
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	// faz o upgrade da conexão pra websocket
 	conn, err := wsUpgrader.Upgrade(w, r, nil)
@@ -77,6 +126,13 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	player.Id = uuid.New()
 	player.Socket = conn
 
+	player.CharType = "002"
+	player.Direction = 3
+	player.X = 3;
+	player.Y = 4;
+	player.MovementDelay = 200;
+	player.LastMovementTime = time.Now();
+
 	Players = append(Players, player)
 
 	debug(fmt.Sprintf("New player: %v", player))
@@ -87,17 +143,18 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	go func() {
 		for _, p := range Players {
 			if p.Socket.RemoteAddr() != player.Socket.RemoteAddr() {
-				if err = player.send(p.position(true)); err != nil {
+				if err = player.send(p.createPositionMessage(true)); err != nil {
 					debug(fmt.Sprintf("Error on send command: %v", err))
 				}
 
-				if err = p.send(player.position(true)); err != nil {
+				if err = p.send(player.createPositionMessage(true)); err != nil {
 					debug(fmt.Sprintf("Error on send command: %v", err))
 				}
 			}
 		}
 	}()
 
+	// listen para comandos ou erros
 	debug("Published")
 
 	for {
@@ -113,7 +170,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 				} else {
 					debug(fmt.Sprintf("Destroy player: %v", player))
 
-					if err = p.send(Message{Online: false, Id: player.Id}); err != nil {
+					if err = p.send(PlayerPositionMessage{Online: false, Id: player.Id}); err != nil {
 						debug(fmt.Sprintf("Error on send command: %v", err))
 					}
 				}
@@ -134,12 +191,74 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 			messageDataType := messageData["type"]
 
 			if messageDataType == "pos" {
-				if value, ok := messageData["x"]; ok {
-					player.X = int(value.(float64))
+				// ++++++++++++++++++++++++++++++++++++++++++
+				// pos = posição do personagem
+				// ++++++++++++++++++++++++++++++++++++++++++
+
+				if player.canMove() {
+					player.updateLastMovementTime()
+
+					if value, ok := messageData["x"]; ok {
+						player.X = int(value.(float64))
+					}
+
+					if value, ok := messageData["y"]; ok {
+						player.Y = int(value.(float64))
+					}
+
+					if err = player.send(player.createSimpleMessage("pos-ok")); err != nil {
+						debug(fmt.Sprintf("Error on send command: %v", err))
+					}
+				} else {
+					if err = player.send(player.createInvalidPositionMessage()); err != nil {
+						debug(fmt.Sprintf("Error on send command: %v", err))
+					}
+				}
+			} else if messageDataType == "login" {
+				// ++++++++++++++++++++++++++++++++++++++++++
+				// login = pedido de login
+				// ++++++++++++++++++++++++++++++++++++++++++
+
+				var username = ""
+				var password = ""
+				var version = ""
+
+				if value, ok := messageData["username"]; ok {
+					username = value.(string)
 				}
 
-				if value, ok := messageData["y"]; ok {
-					player.Y = int(value.(float64))
+				if value, ok := messageData["password"]; ok {
+					password = value.(string)
+				}
+
+				if value, ok := messageData["version"]; ok {
+					version = value.(string)
+				}
+
+				if version != appVersion {
+					debug(fmt.Sprintf("Player is trying use a different version: %v", version))
+
+					if err = player.send(player.createSimpleMessage("version-invalid")); err != nil {
+						debug(fmt.Sprintf("Error on send command: %v", err))
+					}
+
+					player.Socket.Close()
+				}
+
+				if username == "demo" && password == "demo" {
+					debug(fmt.Sprintf("Player do login with success: %v - %v", username, password))
+
+					if err = player.send(player.createPlayerDataMessage()); err != nil {
+						debug(fmt.Sprintf("Error on send command: %v", err))
+					}
+				} else {
+					debug(fmt.Sprintf("Player is trying do login with a invalid username and password: %v - %v", username, password))
+
+					if err = player.send(player.createSimpleMessage("login-invalid")); err != nil {
+						debug(fmt.Sprintf("Error on send command: %v", err))
+					}
+
+					player.Socket.Close()
 				}
 			}
 		}
@@ -147,7 +266,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		go func() {
 			for _, p := range Players {
 				if p.Id != player.Id {
-					if err = p.send(player.position(false)); err != nil {
+					if err = p.send(player.createPositionMessage(false)); err != nil {
 						debug(fmt.Sprintf("Error on send command: %v", err))
 					}
 				}
